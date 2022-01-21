d9d2e87d811f0d3c8e47dd2990bf24b454b978a5
Victor Carvalho <victor.blq@gmail.com>
2022-01-21 12:41:10 -0300

    Improving the configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -16,9 +16,13 @@
   #Install virtualbox extension pack
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      version = 2;
+      device = "/dev/sda";
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
@@ -29,17 +33,6 @@
   networking.useDHCP = false;
   networking.interfaces.enp0s3.useDHCP = true;
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # Enable the X11 windowing system.
-  #services.xserver.enable = true;
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
   services = {
     gnome.gnome-keyring.enable = true;
 
@@ -47,26 +40,9 @@
       enable = true;
       layout = "us";
       xkbVariant = "intl";
-      videoDrivers = ["nvidia"];
 
       displayManager.defaultSession = "none+bspwm";
 
-     # displayManager.sessionCommands = ''
-      #  ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr &
-       # ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
-        #  Xcursor.theme: Adwaita
-         # Xcursor.size: 24
-       # EOF
-     # '';
-
-      #displayManager.lightdm = {
-      #  enable = true;
-#	      greeters.mini = {
-#	        enable = true;
-#	        user = "victor";
-#	      };
- #     };
-      
       windowManager.bspwm = {
         enable = true;
       };
@@ -74,11 +50,6 @@
 
   };
   
-  #services.udev.packages = with pkgs; [gnome3.gnome-settings-daemon];
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
@@ -102,22 +73,40 @@
     google-chrome
     emacs
     terminator
+    alacritty
   ];
 
-  #environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome-photos pkgs.gnome.gnome-music pkgs.gnome.gnome-terminal pkgs.gnome.gedit pkgs.epiphany pkgs.evince pkgs.gnome.gnome-characters pkgs.gnome.totem pkgs.gnome.tali pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour pkgs.gnome.geary ];
+  programs = {
+    zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        
+      };
+    };
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  #Fonts
+  #fonts = {
+   # fonts = with pkgs; [
+    #  emacs-all-the-icons-fonts
+     # hack-font
+     # roboto
+     # roboto-mono
+     # ibm-plex
+    #];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  #services.openssh.enable = true;
+    #fontconfig = {
+     # defaultFonts = {
+      #  sansSerif = ["roboto" 
+       #              "ibm-plex"];
+       # monospace = ["roboto-mono" 
+       #              "ibm-plex"];
+     # };
+    #};
+ # };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
