571ec90548f825a9939fea37b2111f5317015b6a
Victor Carvalho <victor.blq@gmail.com>
2026-01-19 17:38:50 -0300

    Modify zsh-full.sh to comment out installation steps
    Comment out zsh installation and config symlink.

--- scripts/zsh-full.sh
+++ scripts/zsh-full.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install zsh
+# sudo apt install zsh
 
 cat /etc/shells
 
@@ -11,7 +11,7 @@ DOTFILESDIR=${HOME}/.dotfiles
 ${DOTFILESDIR}/scripts/./install-fzf.sh
 ${DOTFILESDIR}/scripts/./install-oh-my-zsh.sh
 ln -sf ${DOTFILESDIR}/.oh-my-zsh/custom/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-ln -sf ${DOTFILESDIR}/.oh-my-zsh/custom/config.zsh ~/.oh-my-zsh/custom/config.zsh
+# ln -sf ${DOTFILESDIR}/.oh-my-zsh/custom/config.zsh ~/.oh-my-zsh/custom/config.zsh
 ${DOTFILESDIR}/scripts/./install-oh-my-zsh-plugins.sh
 ln -sf ${DOTFILESDIR}/.p10k.zsh ~/.p10k.zsh
 ln -sf ${DOTFILESDIR}/.fzf.zsh ~/.fzf.zsh
