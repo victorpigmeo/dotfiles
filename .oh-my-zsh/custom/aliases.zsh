5dadf205f5f040ec9a8d89ddf7c64c67f66611c5
Victor Carvalho <victor.blq@gmail.com>
2026-01-19 17:35:19 -0300

    Remove unused aliases from aliases.zsh
    Removed aliases for updating clojure-lsp, refreshing AWS credentials, and connecting VPN.

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,9 +1,6 @@
 #!/usr/bin/env zsh
 
 alias c='clear'
-alias update-clojure-lsp='bash <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install) --dir ~/dev/clojure-lsp/ --version nightly --static'
-alias creds='nu aws shared-role-credentials refresh --interactive --deps'
-alias vpn='sudo -E gpclient connect zta.nubank.world'
 alias pio='/home/victor/.platformio/penv/bin/pio'
 
 function j(){
